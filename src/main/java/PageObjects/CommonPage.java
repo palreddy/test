843c59a69f3c7096408e3092873a75b24a8d2c0e
palreddy <shwetha.reddy.22@github.com>
2023-04-13 23:12:27 +0100

    cleaning the code

--- src/main/java/PageObjects/CommonPage.java
+++ src/main/java/PageObjects/CommonPage.java
@@ -3,10 +3,8 @@ package PageObjects;
 import Utilities.Utils;
 import factory.DriverFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
+
 import java.util.concurrent.TimeUnit;
 
 
@@ -36,15 +34,6 @@ public class CommonPage extends DriverFactory {
         driver.quit();
     }
 
-    public void webdriverWait(By by) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10L));
-
-    }
-    public void webdriverWait() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(50));
-        wait.until(webDriver -> "complete".equals(((JavascriptExecutor) webDriver)
-                .executeScript("return document.readyState")));
-    }
 
     public void click(By by) {
         driver.findElement(by).click();
@@ -57,7 +46,6 @@ public class CommonPage extends DriverFactory {
     public String getSearchResults(By searchSucess, By searchFailure) throws InterruptedException {
         String result;
         Thread.sleep(2000);
-
         if ((driver.getPageSource().contains("Sorry, we couldn't find a car with that registration. Please re-enter your registration number")) ||
                 (driver.getPageSource().contains("Our history checks show the MOT has expired."))) {
             result = driver.findElement(searchFailure).getText();
